0bcd410c8fc9c6fbbe190270e170941c9e9eaacf
Zach Lankton <zachlankton@gmail.com>
2022-09-16 14:42:13 +0000

    created demo todo using svelte and couchdb

---
+++ src/components/Todo.svelte
@@ -0,0 +1,555 @@
+<script>
+  const ENTER_KEY = 13;
+  const ESCAPE_KEY = 27;
+
+  let currentFilter = "all";
+  let items = [];
+  let editing = null;
+
+  async function fetchAll() {
+    try {
+      const resp = await fetch("/api/allDocs");
+      items = await resp.json();
+    } catch (err) {
+      items = [];
+    }
+  }
+  fetchAll();
+
+  const updateView = () => {
+    currentFilter = "all";
+    if (window.location.hash === "#/active") {
+      currentFilter = "active";
+    } else if (window.location.hash === "#/completed") {
+      currentFilter = "completed";
+    }
+  };
+
+  window.addEventListener("hashchange", updateView);
+  updateView();
+
+  async function clearCompleted() {
+    const itemsToDelete = items.filter((item) => item.completed);
+    itemsToDelete.map((item) => save({ _deleted: true, ...item }));
+    items = items.filter((item) => !item.completed);
+  }
+
+  function remove(index) {
+    const itemToDelete = items[index];
+    save({ _deleted: true, ...itemToDelete });
+    items = items.slice(0, index).concat(items.slice(index + 1));
+  }
+
+  function toggleAll(event) {
+    items = items.map((item) => ({
+      id: item.id,
+      description: item.description,
+      completed: event.target.checked,
+    }));
+
+    items.map((item) => save(item));
+  }
+
+  async function createNew(event) {
+    if (event.which === ENTER_KEY) {
+      const newItem = {
+        id: uuid(),
+        description: event.target.value,
+        completed: false,
+      };
+      const { error } = await save(newItem);
+      if (!error) {
+        items = items.concat(newItem);
+        event.target.value = "";
+      }
+    }
+  }
+
+  async function save(doc) {
+    try {
+      const resp = await fetch("/api/upsertDoc", {
+        method: "POST",
+        body: JSON.stringify({
+          ...doc,
+        }),
+      });
+      const data = await resp.json();
+      return { data, error: null };
+    } catch (error) {
+      return { data: null, error };
+    }
+  }
+
+  function handleEdit(event) {
+    if (event.which === ENTER_KEY) event.target.blur();
+    else if (event.which === ESCAPE_KEY) editing = null;
+  }
+
+  function submit(event) {
+    items[editing].description = event.target.value;
+    save(items[editing]);
+    editing = null;
+  }
+
+  function uuid() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+      /[xy]/g,
+      function (c) {
+        var r = (Math.random() * 16) | 0,
+          v = c == "x" ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      }
+    );
+  }
+
+  $: filtered =
+    currentFilter === "all"
+      ? items
+      : currentFilter === "completed"
+      ? items.filter((item) => item.completed)
+      : items.filter((item) => !item.completed);
+
+  $: numActive = items.filter((item) => !item.completed).length;
+
+  $: numCompleted = items.filter((item) => item.completed).length;
+</script>
+
+<div class="todoapp-body">
+  <div class="todoapp">
+    <header class="header">
+      <h1>CouchDB Todos</h1>
+      <input
+        class="new-todo"
+        on:keydown={createNew}
+        placeholder="What needs to be done?"
+      />
+    </header>
+
+    {#if items.length > 0}
+      <section class="main">
+        <input
+          id="toggle-all"
+          class="toggle-all"
+          type="checkbox"
+          on:change={toggleAll}
+          checked={numCompleted === items.length}
+        />
+        <label for="toggle-all">Mark all as complete</label>
+
+        <ul class="todo-list">
+          {#each filtered as item, index (item.id)}
+            <li
+              class="{item.completed ? 'completed' : ''} {editing === index
+                ? 'editing'
+                : ''}"
+            >
+              <div class="view">
+                <input
+                  id={item.id}
+                  class="toggle"
+                  type="checkbox"
+                  bind:checked={item.completed}
+                  on:change={() => save(item)}
+                />
+                <label for={item.id} on:dblclick={() => (editing = index)}
+                  >{item.description}</label
+                >
+                <button on:click={() => remove(index)} class="destroy" />
+              </div>
+
+              {#if editing === index}
+                <input
+                  value={item.description}
+                  id="edit"
+                  class="edit"
+                  on:keydown={handleEdit}
+                  on:blur={submit}
+                />
+              {/if}
+            </li>
+          {/each}
+        </ul>
+
+        <footer class="footer">
+          <span class="todo-count">
+            <strong>{numActive}</strong>
+            {numActive === 1 ? "item" : "items"} left
+          </span>
+
+          <ul class="filters">
+            <li>
+              <a class={currentFilter === "all" ? "selected" : ""} href="#/"
+                >All</a
+              >
+            </li>
+            <li>
+              <a
+                class={currentFilter === "active" ? "selected" : ""}
+                href="#/active">Active</a
+              >
+            </li>
+            <li>
+              <a
+                class={currentFilter === "completed" ? "selected" : ""}
+                href="#/completed">Completed</a
+              >
+            </li>
+          </ul>
+
+          {#if numCompleted}
+            <button class="clear-completed" on:click={clearCompleted}>
+              Clear completed
+            </button>
+          {/if}
+        </footer>
+      </section>
+    {/if}
+  </div>
+</div>
+
+<style>
+  button {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    background: none;
+    font-size: 100%;
+    vertical-align: baseline;
+    font-family: inherit;
+    font-weight: inherit;
+    color: inherit;
+    -webkit-appearance: none;
+    appearance: none;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  .todoapp-body {
+    font: 14px "Helvetica Neue", Helvetica, Arial, sans-serif;
+    line-height: 1.4em;
+    background: #f5f5f5;
+    color: #4d4d4d;
+    min-width: 230px;
+    max-width: 550px;
+    margin: 0 auto;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-weight: 300;
+  }
+
+  :focus {
+    outline: 0;
+  }
+
+  .todoapp {
+    background: #fff;
+    margin: 200px 0 40px 0;
+    position: relative;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1);
+  }
+
+  .todoapp input::-webkit-input-placeholder {
+    font-style: italic;
+    font-weight: 300;
+    color: #e6e6e6;
+  }
+
+  .todoapp input::-moz-placeholder {
+    font-style: italic;
+    font-weight: 300;
+    color: #e6e6e6;
+  }
+
+  .todoapp input::input-placeholder {
+    font-style: italic;
+    font-weight: 300;
+    color: #e6e6e6;
+  }
+
+  .todoapp h1 {
+    position: absolute;
+    top: -90px;
+    width: 100%;
+    font-size: 50px;
+    font-weight: 100;
+    text-align: center;
+    color: rgba(175, 47, 47, 0.15);
+    -webkit-text-rendering: optimizeLegibility;
+    -moz-text-rendering: optimizeLegibility;
+    text-rendering: optimizeLegibility;
+  }
+
+  .new-todo,
+  .edit {
+    position: relative;
+    margin: 0;
+    width: 100%;
+    font-size: 24px;
+    font-family: inherit;
+    font-weight: inherit;
+    line-height: 1.4em;
+    border: 0;
+    color: inherit;
+    padding: 6px;
+    border: 1px solid #999;
+    box-shadow: inset 0 -1px 5px 0 rgba(0, 0, 0, 0.2);
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  .new-todo {
+    padding: 16px 16px 16px 60px;
+    border: none;
+    background: rgba(0, 0, 0, 0.003);
+    box-shadow: inset 0 -2px 1px rgba(0, 0, 0, 0.03);
+  }
+
+  .main {
+    position: relative;
+    z-index: 2;
+    border-top: 1px solid #e6e6e6;
+  }
+
+  .toggle-all {
+    width: 1px;
+    height: 1px;
+    border: none; /* Mobile Safari */
+    opacity: 0;
+    position: absolute;
+    right: 100%;
+    bottom: 100%;
+  }
+
+  .toggle-all + label {
+    width: 60px;
+    height: 34px;
+    font-size: 0;
+    position: absolute;
+    top: -52px;
+    left: -13px;
+    -webkit-transform: rotate(90deg);
+    transform: rotate(90deg);
+  }
+
+  .toggle-all + label:before {
+    content: "❯";
+    font-size: 22px;
+    color: #e6e6e6;
+    padding: 10px 27px 10px 27px;
+  }
+
+  .toggle-all:checked + label:before {
+    color: #737373;
+  }
+
+  .todo-list {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  .todo-list li {
+    position: relative;
+    font-size: 24px;
+    border-bottom: 1px solid #ededed;
+  }
+
+  .todo-list li:last-child {
+    border-bottom: none;
+  }
+
+  .todo-list li.editing {
+    border-bottom: none;
+    padding: 0;
+  }
+
+  .todo-list li.editing .edit {
+    display: block;
+    width: calc(100% - 43px);
+    padding: 12px 16px;
+    margin: 0 0 0 43px;
+  }
+
+  .todo-list li.editing .view {
+    display: none;
+  }
+
+  .todo-list li .toggle {
+    text-align: center;
+    width: 40px;
+    /* auto, since non-WebKit browsers doesn't support input styling */
+    height: auto;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    margin: auto 0;
+    border: none; /* Mobile Safari */
+    -webkit-appearance: none;
+    appearance: none;
+  }
+
+  .todo-list li .toggle {
+    opacity: 0;
+  }
+
+  .todo-list li .toggle + label {
+    /*
+		Firefox requires `#` to be escaped - https://bugzilla.mozilla.org/show_bug.cgi?id=922433
+		IE and Edge requires *everything* to be escaped to render, so we do that instead of just the `#` - https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/7157459/
+	*/
+    background-image: url("data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%2240%22%20height%3D%2240%22%20viewBox%3D%22-10%20-18%20100%20135%22%3E%3Ccircle%20cx%3D%2250%22%20cy%3D%2250%22%20r%3D%2250%22%20fill%3D%22none%22%20stroke%3D%22%23ededed%22%20stroke-width%3D%223%22/%3E%3C/svg%3E");
+    background-repeat: no-repeat;
+    background-position: center left;
+  }
+
+  .todo-list li .toggle:checked + label {
+    background-image: url("data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%2240%22%20height%3D%2240%22%20viewBox%3D%22-10%20-18%20100%20135%22%3E%3Ccircle%20cx%3D%2250%22%20cy%3D%2250%22%20r%3D%2250%22%20fill%3D%22none%22%20stroke%3D%22%23bddad5%22%20stroke-width%3D%223%22/%3E%3Cpath%20fill%3D%22%235dc2af%22%20d%3D%22M72%2025L42%2071%2027%2056l-4%204%2020%2020%2034-52z%22/%3E%3C/svg%3E");
+  }
+
+  .todo-list li label {
+    word-break: break-all;
+    padding: 15px 15px 15px 60px;
+    display: block;
+    line-height: 1.2;
+    transition: color 0.4s;
+  }
+
+  .todo-list li.completed label {
+    color: #d9d9d9;
+    text-decoration: line-through;
+  }
+
+  .todo-list li .destroy {
+    display: none;
+    position: absolute;
+    top: 0;
+    right: 10px;
+    bottom: 0;
+    width: 40px;
+    height: 40px;
+    margin: auto 0;
+    font-size: 30px;
+    color: #cc9a9a;
+    margin-bottom: 11px;
+    transition: color 0.2s ease-out;
+  }
+
+  .todo-list li .destroy:hover {
+    color: #af5b5e;
+  }
+
+  .todo-list li .destroy:after {
+    content: "×";
+  }
+
+  .todo-list li:hover .destroy {
+    display: block;
+  }
+
+  .todo-list li .edit {
+    display: none;
+  }
+
+  .todo-list li.editing:last-child {
+    margin-bottom: -1px;
+  }
+
+  .footer {
+    color: #777;
+    padding: 10px 15px;
+    height: 20px;
+    text-align: center;
+    border-top: 1px solid #e6e6e6;
+  }
+
+  .footer:before {
+    content: "";
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    height: 50px;
+    overflow: hidden;
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2), 0 8px 0 -3px #f6f6f6,
+      0 9px 1px -3px rgba(0, 0, 0, 0.2), 0 16px 0 -6px #f6f6f6,
+      0 17px 2px -6px rgba(0, 0, 0, 0.2);
+  }
+
+  .todo-count {
+    float: left;
+    text-align: left;
+  }
+
+  .todo-count strong {
+    font-weight: 300;
+  }
+
+  .filters {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    position: absolute;
+    right: 0;
+    left: 0;
+  }
+
+  .filters li {
+    display: inline;
+  }
+
+  .filters li a {
+    color: inherit;
+    margin: 3px;
+    padding: 3px 7px;
+    text-decoration: none;
+    border: 1px solid transparent;
+    border-radius: 3px;
+  }
+
+  .filters li a:hover {
+    border-color: rgba(175, 47, 47, 0.1);
+  }
+
+  .filters li a.selected {
+    border-color: rgba(175, 47, 47, 0.2);
+  }
+
+  .clear-completed,
+  .todoapp-body .clear-completed:active {
+    float: right;
+    position: relative;
+    line-height: 20px;
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  .clear-completed:hover {
+    text-decoration: underline;
+  }
+
+  /*
+	Hack to remove background from Mobile Safari.
+	Can't use it globally since it destroys checkboxes in Firefox
+*/
+  @media screen and (-webkit-min-device-pixel-ratio: 0) {
+    .toggle-all,
+    .todo-list li .toggle {
+      background: none;
+    }
+
+    .todo-list li .toggle {
+      height: 40px;
+    }
+  }
+
+  @media (max-width: 430px) {
+    .footer {
+      height: 50px;
+    }
+
+    .filters {
+      bottom: 10px;
+    }
+  }
+</style>
