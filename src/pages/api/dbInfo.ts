6cb60d76d522fd28c4b5f25b71beb9098493e3d7
Zach Lankton <zachlankton@gmail.com>
2022-09-20 17:57:00 +0000

    allow api endpoints to have custom db header

--- src/pages/api/dbInfo.ts
+++ src/pages/api/dbInfo.ts
@@ -1,7 +1,9 @@
 import db from "../../db/db";
 
-export async function get() {
-  const data = await db.getDbInfo();
+export async function get({ request }) {
+  const dbName = request.headers.get("db_name") || import.meta.env.DB_NAME;
+
+  const data = await db.getDbInfo({ dbName });
 
   return new Response(JSON.stringify(data), {
     status: 200,
--- src/pages/api/getDoc/[id].ts
+++ src/pages/api/getDoc/[id].ts
@@ -1,7 +1,9 @@
 import db from "../../../db/db";
 
-export async function get({ params }) {
-  const data = await db.getDoc(params.id);
+export async function get({ params, request }) {
+  const dbName = request.headers.get("db_name") || import.meta.env.DB_NAME;
+
+  const data = await db.getDoc(params.id, { dbName });
 
   return new Response(JSON.stringify(data), {
     status: 200,
